a3fe22de553232f0b9500c887821979fdd1f0dd1
qiwentao <qiwentao@MacdeiMac.local>
2018-07-11 10:43:39 +0800

    feat: 添加autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   plugins: [
     require('precss'),
-    require('postcss-cssnext')({ browsers: ['> 1%', 'last 2 versions'] }),
+    require('postcss-cssnext'),
+    require('autoprefixer')({
+      browsers: ['> 0.5%', 'last 4 versions', 'Firefox ESR', 'ios > 9', 'android > 4.4'],
+      flexbox: 'no-2009',
+    }),
   ],
 };
